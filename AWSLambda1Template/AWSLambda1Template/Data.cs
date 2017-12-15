1ec8c61ea566795d7f8fbaf23ff308e3f463e60a
dmnbc <dmnbc@detlef-kremer.com>
2017-12-15 12:13:56 +0100

    init

---
+++ AWSLambda1Template/AWSLambda1Template/Data.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AWSLambda1Template
+{
+    public class Data
+    {
+        public string language;
+
+
+        public Data(string language)
+        {
+            this.language = language;
+        }
+    }
+}
