636022128f0a525de9bd883850f4a29c8bf3d787
dmnbc <dmnbc@detlef-kremer.com>
2017-12-29 12:40:50 +0100

    Projekt wurde ausgesetzt. Diverse Probleme mit fehlenden Assemblies

--- OnLaunchAWSLambda/OnLaunchAWSLambda/Function.cs
+++ OnLaunchAWSLambda/OnLaunchAWSLambda/Function.cs
@@ -1,9 +1,3 @@
-using System;
-using System.IO;
-
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 /*   folgende NuGets sind zu laden
     
      Newtonsoft.Json" Version="10.0.1"   keine höheren Versionen !!!
@@ -13,23 +7,26 @@ using System.Threading.Tasks;
  * 
  */
 
+using System;
 using Alexa.NET.Request;          // erfordert NuGet Pakate   Alexa.Net Tim Heuer
 using Alexa.NET.Request.Type;     // getestet mit Version 1.4.0
 using Alexa.NET.Response;
 using Amazon.Lambda.Core;
 
 // S3
-using Amazon.S3;
-using Amazon.S3.Transfer;
-using Amazon.S3.Model;
+//using Amazon.S3;
+//using Amazon.S3.Transfer;
+//using Amazon.S3.Model;
 
+// DB Connect via Oracle Connector
 
+using MySql.Data.MySqlClient;   // dll durch 'Verweis hinzufügen' einbinden 
 
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
-          // gleich dem Projektnamen
+// gleich dem Projektnamen
 namespace OnLaunchAWSLambda
 {
     public class Function
@@ -37,19 +34,28 @@ namespace OnLaunchAWSLambda
         public static string skillName = "OnLaunchAWSLambda";
         public static bool logging = true;
 
-        public SkillResponse FunctionHandler(SkillRequest input, ILambdaContext context)
+        public SkillResponse FunctionHandlerAsync(SkillRequest input, ILambdaContext context)
         {
-                     // Objekte vor Aufruf erzeugen
-        SkillResponse  skillResponse = new SkillResponse();
-                       skillResponse.Response = new ResponseBody{ShouldEndSession = false};
-        IOutputSpeech  outputSpeech = null;
-        IntentRequest  intentRequest = input.Request as IntentRequest;
-        ILambdaLogger  lambdaLogger = context.Logger;   // log to CloudWatch
+            // Objekte vor Aufruf erzeugen
+            SkillResponse skillResponse = new SkillResponse();
+            skillResponse.Response = new ResponseBody { ShouldEndSession = false };
+            IOutputSpeech outputSpeech = null;
+            IntentRequest intentRequest = input.Request as IntentRequest;
+            ILambdaLogger lambdaLogger = context.Logger;   // log to CloudWatch
 
-            //S3
-            
-           
-             if (logging)
+
+            //S3   to be evaluated. 
+
+            //  db Connect , problem mit nicht passenden assemblys
+            string dbconnectstring =StaticValues.SERVER+StaticValues.DATABASE+StaticValues.UID+StaticValues.PASSWORD;
+            MySqlConnection connection = new MySqlConnection(dbconnectstring);
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM Waren";
+            MySqlDataReader Reader;
+            connection.Open();
+            Reader = command.ExecuteReader();
+
+            if (logging)
             {
                 lambdaLogger.LogLine("Log started");
                 lambdaLogger.LogLine("FunctionName:" + context.FunctionName + "last edited by " + StaticValues.company);
